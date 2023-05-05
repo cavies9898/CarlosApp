0037dc93c1e5829986109b9d69cbc33ea389f15e
Carlos Villanueva <carlos.villanueva@s2next.com>
2023-05-05 12:24:52 -0600

    Master: Set New Project

--- app/src/main/java/com/example/carlosapp/DogsResponse.kt
+++
@@ -1,8 +0,0 @@
-package com.example.carlosapp
-
-import com.google.gson.annotations.SerializedName
-
-data class DogsResponse(
-    @SerializedName("status") var status: String,
-    @SerializedName("message") var images: List<String>
-)
